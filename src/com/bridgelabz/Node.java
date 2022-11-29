a9f4ac79a74e3b1a025280714e72b12006f85a47
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-11-29 07:41:21 +0530

    Add: appending nodes

--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -1,27 +1,27 @@
 package com.bridgelabz;
 
 public class Node<K> {
-        private K data;
-        private Node next;
+    private K data;
+    private Node next;
 
-        public Node(K data) {
-            this.data = data;
-            this.next = null;
-        }
-
-        public K getData() {
-            return data;
-        }
-
-        public void setData(K data) {
-            this.data = data;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
+    public Node(K data) {
+        this.data = data;
+        this.next = null;
     }
+
+    public K getData() {
+        return data;
+    }
+
+    public void setData(K data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
